ac8993c07e6abcc052ef23cc8ed624f0de938f62
sujit-168 <su2054552689@gmail.com>
2026-02-09 09:31:44 +0800

    Add GitHub Action CI for DEB building and update generate.sh to support multiple OS versions

--- generate.sh
+++ generate.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+# 获取系统版本参数，默认为 jammy
+OS_VERSION=${1:-jammy}
+ROS_DISTRO=${2:-noetic}
+
 # 遍历当前目录及其子目录
 find . -type f -name 'package.xml' | while read -r package_xml; do
     # 获取包含 package.xml 的目录路径
     dir=$(dirname "$package_xml")
     
-    echo "在目录 $dir 中发现了 package.xml，执行 bloom-generate 命令..."
+    echo "在目录 $dir 中发现了 package.xml，执行 bloom-generate 命令 (OS: $OS_VERSION, Distro: $ROS_DISTRO)..."
     
     # 执行 bloom-generate 命令
-   bash -c " cd $dir &&  bloom-generate rosdebian --os-name ubuntu --os-version jammy --ros-distro noetic"
+    (cd "$dir" && bloom-generate rosdebian --os-name ubuntu --os-version "$OS_VERSION" --ros-distro "$ROS_DISTRO")
 done
